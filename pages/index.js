78cdd9daedebb9f2b59af81a32643d038fd3f214
Audun Nilsen <audunni86@gmail.com>
2021-07-07 11:39:42 +0200

    fixed retaining scrolling on page change

--- pages/index.js
+++ pages/index.js
@@ -7,37 +7,35 @@ import Link from 'next/link';
 import Card from '@/components/Gallery/Card.js';
 import Image from '@/components/Gallery/Image.js';
 import Container from '@/components/Container.js';
-import UserContext from '@/components/Scrollcontext'
+import UserContext from '@/components/Scrollcontext';
 import { useContext, useEffect } from 'react';
+import { useRouter } from 'next/router';
 
 
 const Home = ({result = []}) => {
-
   const { scrollRef } = useContext(UserContext);
+  const router = useRouter()
+  
+  const handleScrollPos = (url) => {
+    //every time the window is scrolled, update the reference. This will not cause a re-render, meaning smooth uninterrupted scrolling.
+    scrollRef.current.scrollPos = window.scrollY
+  };
+  useEffect(() => {
+    window.scrollTo(0, scrollRef.current.scrollPos);}, []);
 
   useEffect(() => {
-    //called when the component has been mounted, sets the scroll to the currently stored scroll position
-    window.scrollTo(0, scrollRef.current.scrollPos);
-
-    const handleScrollPos = () => {
-      //every time the window is scrolled, update the reference. This will not cause a re-render, meaning smooth uninterrupted scrolling.
-      scrollRef.current.scrollPos = window.scrollY
-    };
-
-    window.addEventListener('scroll', handleScrollPos);
-
+    router.events.on('routeChangeStart', handleScrollPos)
     return () => {
-      //remove event listener on unmount
-      window.removeEventListener('scroll', handleScrollPos);
-    };
-    });
+      router.events.off('routeChangeStart', handleScrollPos)
+    }
+  }, [router.asPath])
 
     return (
       <Layout title='Ranveig M Nilsen - Hjem'>
         <Container>
             <Grid>           
                 {result.map(({_id, imageUrl, slug, title}) => (
-                  <Link scroll={false} key={_id} href={{
+                  <Link key={_id} href={{
                       pathname: '/galleri/[slug]',
                       query: { slug: `${slug.current}`}}} passHref>
                       <a><Card>
